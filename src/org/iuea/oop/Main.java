53952ba450c57142165db7fdef196603aab9c250
Gateka <47472522+Gateka@users.noreply.github.com>
2019-10-11 10:29:50 +0200

    Main Updates

--- src/org/iuea/oop/Main.java
+++ src/org/iuea/oop/Main.java
@@ -3,11 +3,14 @@ package org.iuea.oop;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JOptionPane;
+import org.iuea.oop.modal.Students;
 import org.iuea.oop.views.LoginView;
 
 public class Main {
-        public static void main(String[] args) {
+    public static void main(String[] args) {
+            //Students s = new Students();
         LoginView lv = new LoginView();
+        Students s;
         lv.login.addActionListener(new ActionListener(){
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -18,6 +21,18 @@ public class Main {
                     JOptionPane.showMessageDialog(null, "User logged in successfully");
                     lv.userName.setText("");
                     lv.password.setText("");
+                    Students s = new Students();
+                    lv.dispose();
+                    s.item5.addActionListener(new ActionListener(){
+                        @Override
+                        public void actionPerformed(ActionEvent e) {
+                        lv.setVisible(true);
+                        s.setVisible(false);
+                    }
+        
+        
+                    });
+                    
                 } else {
                     JOptionPane.showMessageDialog(null, "Username and password do not match");
                     lv.userName.setText("");
@@ -25,5 +40,6 @@ public class Main {
                 }
             }  
         });
+        
     }
 }
